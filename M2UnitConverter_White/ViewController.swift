a2b3b7fd7030fe9a8f3711e681333bd5d378bf9c
james_white249 <james_white249@mymail.eku.edu>
2026-02-12 13:19:10 -0500

    Initial Commit

--- M2UnitConverter_White/ViewController.swift
+++ M2UnitConverter_White/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var inputText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
